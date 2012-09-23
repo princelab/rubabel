8ab8db1dd8a41f83db624ee6b90697983659b902
John Prince <jtprince@gmail.com>
2012-09-23 02:42:15 -0600

    reworking fragmentable

--- lib/rubabel/molecule/fragmentable.rb
+++ lib/rubabel/molecule/fragmentable.rb
@@ -7,18 +7,20 @@ module Rubabel
     module Fragmentable
 
       #:sp3c_oxygen_asymmetric_far_sp3, :sp3c_nitrogen_asymmetric_far_sp3,
-      RULES = Set[ :alcohol_to_aldehyde, :peroxy_to_carboxy, :co2_loss, 
-        :sp3c_oxygen_double_bond_far_side_sp3, :sp3c_oxygen_double_bond_far_side_sp2, :sp3c_oxygen_double_bond_water_loss, :sp3c_nitrogen_double_bond,
-      ]
+      #RULES = Set[ :alcohol_to_aldehyde, :peroxy_to_carboxy, :co2_loss, 
+      #  :sp3c_oxygen_double_bond_far_side_sp3, :sp3c_oxygen_double_bond_far_side_sp2, :sp3c_oxygen_double_bond_water_loss, :sp3c_nitrogen_double_bond,
+      #]
       #ADDUCTS = [:lioh, :nh4cl, :nh4oh]
-      CO_RULES = Set[:alcohol_to_aldehyde, :peroxy_to_carboxy, :co2_loss, 
-        :sp3c_oxygen_double_bond_water_loss, :sp3c_oxygen_double_bond_far_side_sp2, :sp3c_oxygen_double_bond_far_side_sp3, :sp3c_oxygen_asymmetric_far_sp3
-      ]
+      #CO_RULES = Set[:alcohol_to_aldehyde, :peroxy_to_carboxy, :co2_loss, 
+      #  :sp3c_oxygen_double_bond_water_loss, :sp3c_oxygen_double_bond_far_side_sp2, :sp3c_oxygen_double_bond_far_side_sp3, :sp3c_oxygen_asymmetric_far_sp3
+      #]
+
+      RULES = Set[:cad_o, :cad_oo]
 
       DEFAULT_OPTIONS = {
         rules: RULES,
         #adduct: nil,
-        ph: 7.4,
+        #ph: 7.4,
         # return only the set of unique fragments
         uniq: false, 
       }
@@ -142,98 +144,34 @@ module Rubabel
 
       end
 
-      # to ensure proper fragmentation, will add_h!(ph) first at the given ph
       # an empty array is returned if there are no fragments generated.
+      # Hydrogens are added at a pH of 7.4, unless they have already been
+      # added.
       #
-      #     :ph => 7.4
-      #     :uniq => false 
+      #     :rules => queryable by :include? set of rules
+      #     :uniq => false
       def fragment(opts={})
+        search_only_uniq_set = true
         opts = DEFAULT_OPTIONS.merge(opts)
         opts[:rules].each do |rule| 
           raise ArgumentError, "bad rule: #{rule}" unless RULES.include?(rule)
         end
 
         had_hydrogens = self.h_added?
-
-        self.correct_for_ph!(opts[:ph])
+        self.correct_for_ph!(7.4) unless had_hydrogens
         self.remove_h!
 
-        rules = opts[:rules]
         fragment_sets = []
-        if rules.any? {|rule| CO_RULES.include?(rule) }
-          putsv "matching C-O"
-          self.each_match("CO").each do |_atoms|
-            # note: this will *not* match C=O
-            (carbon, oxygen) = _atoms
-            carbon_nbrs = carbon.atoms.reject {|atom| atom == oxygen }
-            c3_nbrs = carbon_nbrs.select {|atm| atm.type == 'C3' }
-            # pulling this out here causes it to work incorrectly internally
-            # (not sure why)
-            #co_bond = carbon.get_bond(oxygen)
 
-            case oxygen.bonds.size # non-hydrogen bonds
-            when 1  # *must* be an alcohol or a carboxylic acid
-              putsv "#{csmiles} oxygen has no other bonds besides C-O (alcohol or carboxylic acid)"
-              if carbon.type == 'C3'
-                if rules.include?(:sp3c_oxygen_double_bond_water_loss) 
-                  putsv "rule :sp3c_oxygen_double_bond_water_loss"
-                  fragment_sets.push *near_side_double_bond_break(carbon, oxygen)
-                end
-                if rules.include?(:alcohol_to_aldehyde)
-                    putsv "rule :alcohol_to_aldehyde"
-                  fragment_sets.push *alcohol_to_aldehyde(carbon, oxygen, carbon_nbrs)
-                end
-              elsif carbon.carboxyl_carbon?
-                if rules.include?(:co2_loss)
-                  putsv "rule :co2_loss"
-                  if c3_nbr = c3_nbrs.first
-                    fragment_sets.push *co2_loss(carbon, oxygen, c3_nbr)
-                  end
-                end
-              end
-            when 2
-              putsv "#{csmiles} c-o & oxygen has 2 non-hydrogen bonds"
-              oxygen_nbr = oxygen.atoms.reject {|atom| atom.idx == carbon.idx }.first
-              if carbon.type == 'C3'
-                if rules.include?(:peroxy_to_carboxy)
-                  fragment_sets.push *peroxy_to_carboxy(carbon, oxygen, carbon_nbrs, oxygen_nbr)
-                end
-                # ester and ethers (look *only* on close side for places to make
-                # double bond)
-
-                if oxygen_nbr.type == 'C3'
-                  putsv "oxygen nbr is C3"
-                  if rules.include?(:sp3c_oxygen_double_bond_far_side_sp3) 
-                    putsv "rule :sp3c_oxygen_double_bond_far_side_sp3"
-                    fragment_sets.push *near_side_double_bond_break(carbon, oxygen)
-                  end
-                  if rules.include?(:sp3c_oxygen_asymmetric_far_sp3)
-                    putsv "rule :sp3c_oxygen_asymmetric_far_sp3"
-                    # only returns a single frag set
-                    fragment_sets.push electrophile_snatches_electrons(carbon, oxygen)
-                  end
-                end
-                if oxygen_nbr.type == 'C2'
-                  if rules.include?(:sp3c_oxygen_double_bond_far_side_sp2)
-                    putsv "rule :sp3c_oxygen_double_bond_far_side_sp2"
-                    fragment_sets.push *near_side_double_bond_break(carbon, oxygen)
-                  end
-                end
-                # note: the case of a carboxy is found with CO search
+        if opts[:rules].any? {|r| [:cad_o, :cad_oo].include?(r) }
+          self.each_match("C[O;h1,O]", search_only_uniq_set) do |carbon, oxygen|
+            appendage = oxygen.atoms.find {|a| a.el != :c }
+            self.feint_double_bond(carbon.get_bond(oxygen)) do |mol|
+              carbon.atoms.select {|a| a.el == :c }.each do |carbon_nbr|
+                frag_pair = mol.split(carbon.get_bond(carbon_nbr))
               end
             end
-          end
-        end
-        if rules.include?(:sp3c_nitrogen_double_bond)
-          self.each_match("CN") do |_atoms|
-            (carbon, nitrogen) = _atoms
-            num_nitrogen_bonds = nitrogen.bonds.size
-            case num_nitrogen_bonds
-            when 2
-              if carbon.type == 'C3'
-                fragment_sets.push *near_side_double_bond_break(carbon, nitrogen)
-              end
-            end
+
           end
         end
 
@@ -243,11 +181,109 @@ module Rubabel
         end
         if opts[:uniq]
           # TODO: impelent properly
-          #fragment_sets = fragment_sets.uniq_by(&:csmiles) 
           raise NotImplementedError
+          #fragment_sets = fragment_sets.uniq_by(&:csmiles) 
         end
+
         fragment_sets
       end
+
+
+#        had_hydrogens = self.h_added?
+
+        #self.correct_for_ph!(opts[:ph])
+        #self.remove_h!
+
+        #rules = opts[:rules]
+        #fragment_sets = []
+        #if rules.any? {|rule| CO_RULES.include?(rule) }
+          #putsv "matching C-O"
+          #self.each_match("CO").each do |_atoms|
+            ## note: this will *not* match C=O
+            #(carbon, oxygen) = _atoms
+            #carbon_nbrs = carbon.atoms.reject {|atom| atom == oxygen }
+            #c3_nbrs = carbon_nbrs.select {|atm| atm.type == 'C3' }
+            ## pulling this out here causes it to work incorrectly internally
+            ## (not sure why)
+            ##co_bond = carbon.get_bond(oxygen)
+
+            #case oxygen.bonds.size # non-hydrogen bonds
+            #when 1  # *must* be an alcohol or a carboxylic acid
+              #putsv "#{csmiles} oxygen has no other bonds besides C-O (alcohol or carboxylic acid)"
+              #if carbon.type == 'C3'
+                #if rules.include?(:sp3c_oxygen_double_bond_water_loss) 
+                  #putsv "rule :sp3c_oxygen_double_bond_water_loss"
+                  #fragment_sets.push *near_side_double_bond_break(carbon, oxygen)
+                #end
+                #if rules.include?(:alcohol_to_aldehyde)
+                    #putsv "rule :alcohol_to_aldehyde"
+                  #fragment_sets.push *alcohol_to_aldehyde(carbon, oxygen, carbon_nbrs)
+                #end
+              #elsif carbon.carboxyl_carbon?
+                #if rules.include?(:co2_loss)
+                  #putsv "rule :co2_loss"
+                  #if c3_nbr = c3_nbrs.first
+                    #fragment_sets.push *co2_loss(carbon, oxygen, c3_nbr)
+                  #end
+                #end
+              #end
+            #when 2
+              #putsv "#{csmiles} c-o & oxygen has 2 non-hydrogen bonds"
+              #oxygen_nbr = oxygen.atoms.reject {|atom| atom.idx == carbon.idx }.first
+              #if carbon.type == 'C3'
+                #if rules.include?(:peroxy_to_carboxy)
+                  #fragment_sets.push *peroxy_to_carboxy(carbon, oxygen, carbon_nbrs, oxygen_nbr)
+                #end
+                ## ester and ethers (look *only* on close side for places to make
+                ## double bond)
+
+                #if oxygen_nbr.type == 'C3'
+                  #putsv "oxygen nbr is C3"
+                  #if rules.include?(:sp3c_oxygen_double_bond_far_side_sp3) 
+                    #putsv "rule :sp3c_oxygen_double_bond_far_side_sp3"
+                    #fragment_sets.push *near_side_double_bond_break(carbon, oxygen)
+                  #end
+                  #if rules.include?(:sp3c_oxygen_asymmetric_far_sp3)
+                    #putsv "rule :sp3c_oxygen_asymmetric_far_sp3"
+                    ## only returns a single frag set
+                    #fragment_sets.push electrophile_snatches_electrons(carbon, oxygen)
+                  #end
+                #end
+                #if oxygen_nbr.type == 'C2'
+                  #if rules.include?(:sp3c_oxygen_double_bond_far_side_sp2)
+                    #putsv "rule :sp3c_oxygen_double_bond_far_side_sp2"
+                    #fragment_sets.push *near_side_double_bond_break(carbon, oxygen)
+                  #end
+                #end
+                ## note: the case of a carboxy is found with CO search
+              #end
+            #end
+          #end
+        #end
+        #if rules.include?(:sp3c_nitrogen_double_bond)
+          #self.each_match("CN") do |_atoms|
+            #(carbon, nitrogen) = _atoms
+            #num_nitrogen_bonds = nitrogen.bonds.size
+            #case num_nitrogen_bonds
+            #when 2
+              #if carbon.type == 'C3'
+                #fragment_sets.push *near_side_double_bond_break(carbon, nitrogen)
+              #end
+            #end
+          #end
+        #end
+
+        #unless had_hydrogens
+          #fragment_sets.each {|set| set.each(&:remove_h!) }
+          #self.remove_h!
+        #end
+        #if opts[:uniq]
+          ## TODO: impelent properly
+          ##fragment_sets = fragment_sets.uniq_by(&:csmiles) 
+          #raise NotImplementedError
+        #end
+        #fragment_sets
+      #end
 
     end
     include Fragmentable
--- spec/rubabel/molecule/fragmentable_spec.rb
+++ spec/rubabel/molecule/fragmentable_spec.rb
@@ -21,139 +21,198 @@ describe Rubabel::Molecule::Fragmentable do
       expect { mol.fragment(rules: [:wackiness]) }.to raise_error
     end
 
-    describe 'primary oxygen or peroxide appendage dumping to form C=O' do
-      # rule 1a
+    describe 'cad_o: carbonyl appendage dump ' do
+      # a primary oxygen or peroxide => C=O appendage dump
+      
+      describe 'cad_o: primary alcohol' do
+        mol = Rubabel["NCC(O)CC"]
+        frags = mol.fragment(rules: [:cad_o])
+        frags.flatten(1).map(&:csmiles).should == ["C[NH3+]", "CCC=O", "C([NH3+])C=O", "CC"]
+      end
 
-    end
+      HERE <--> still working on peroxide (need to figure out best way to transfer group)
+      # swap might be the simplest
 
-    describe 'oxygen alpha/beta hydrogen stealing' do
-      # 2
+      describe 'peroxide' do
+        mol = Rubabel["NCC(OO)CC"]
+        frags = mol.fragment(rules: [:cad_oo])
+        frags.flatten(1).each_with_index do |f,i|
+          f.write("mol#{i}.svg")
+        end
+      end
+
+     # describe 'cad_o: carboxylate' do
+        #mol = Rubabel["CCC(=O)O"]
+        #pieces = mol.fragment(rules: [:cad_o])
+        #pieces.size.should == 4
+        #p pieces
+      #end
+
+      #describe 'cad_o: carboxylic acid' do
+        #mol = Rubabel["CCC(=O)O"]
+        #mol.add_h!(1.5)
+        #pieces = mol.fragment(rules: [:cad_o])
+        #pieces.size.should == 4
+        #p pieces
+      #end
+
     end
 
     describe 'oxygen bond stealing' do
       # oxygen just steals the electron pair it is attached to.  This
       # typically results in a negatively charged oxygen and a positively
       # charged carbo-cation.
-    end
-
-
-
-    describe ':sp3c_nitrogen_double_bond' do
-
-      it 'cleaves like an ether a secondary NH group if possible' do
-        mol = Rubabel["CCNC"]
-        frag_sets = mol.fragment(rules: [:sp3c_nitrogen_double_bond])
-        frag_sets.size.should == 1
-        csmiles = frag_sets.first.map(&:csmiles)
-        csmiles.should include("C=C")
-        csmiles.should include("C[NH3+]")
+      describe 'ether to ions' do
       end
 
-      it 'will not cleave if not possible' do
-        mol = Rubabel["CNC"]
-        frag_sets = mol.fragment(rules: [:sp3c_nitrogen_double_bond])
-        frag_sets.should be_empty
+      describe 'ester to ions' do
       end
 
-    end
-
-    describe ':co2_loss' do
-      it 'loss of CO2 from carboxy group with charge transfer' do
-        mol = Rubabel["NCC(=O)O"]
-        frag_sets = mol.fragment( rules: [:co2_loss] )
-        frag_sets.size.should == 1
-        csmiles = frag_sets.first.map(&:csmiles)
-
-        csmiles.should include("[CH2-][NH3+]")
-        csmiles.should include("O=C=O")
+      describe 'carboxyl group' do
       end
 
-      it "doesn't remove CO2 if adjacent is not c3" do
-        mol = Rubabel["C=CC(=O)O"]
-        fragments = mol.fragment( rules: [:co2_loss] )
-        fragments.should be_empty
-      end
-
-    end
-
-    describe ':peroxy_to_carboxy' do
-      it 'works' do
-        mol = Rubabel["NCCC(OO)CC"]
-        frag_sets = mol.fragment( rules: [:peroxy_to_carboxy] )
-        frag_sets.size.should == 2 
-        frag_sets.flatten(1).map(&:csmiles).sort.should == ["CC", "CCC(=O)O", "CC[NH3+]", "OC(=O)CC[NH3+]"]
+      describe 'phosphodiester' do
       end
     end
 
-    describe ':sp3c_oxygen_asymmetric_far_sp3', :pending do
-      it 'splits like sp3c_oxygen_double_bond except oxygen takes the electrons' do
-        $VERBOSE = 3
-        mol = Rubabel["NCCCOCC"]
-        frag_sets = mol.fragment( rules: [:sp3c_oxygen_asymmetric_far_sp3] )
-        $VERBOSE = nil
-        frag_sets.size.should == 2
-        #mol = Rubabel["NCCOCC"]
-        #p mol.fragment( rules: [:sp3c_oxygen_asymmetric_far_sp3] )
-        #mol = Rubabel["NCOC"] 
-        #p mol.fragment( rules: [:sp3c_oxygen_asymmetric_far_sp3] )
-      end
-    end
-
-    describe ':sp3c_oxygen_double_bond_water_loss' do
-
-      it 'does h2o loss of alcohol' do
-        mol = Rubabel["NCCC(O)CC"]
-        fragments = mol.fragment( rules: [:sp3c_oxygen_double_bond_water_loss] )
-        fragments.flatten(1).map(&:csmiles).sort.should == ["CC=CCC[NH3+]", "CCC=CC[NH3+]", "O", "O"]
+    # this is really a subset of oxygen bond stealing: if the negatively
+    # charged oxygen can rip off a nearby proton, it will.
+    describe 'oxygen alpha/beta/gamma hydrogen stealing' do
+      describe 'primary alcohol giving water loss' do
       end
 
-      it 'h2o loss does not allow bad chemistry' do
-        # lone pair and double bond resonance ?
-        mol = Rubabel["NCC(O)CC"]
-        fragments = mol.fragment( rules: [:sp3c_oxygen_double_bond_water_loss] )
-        fragments.flatten(1).map(&:csmiles).sort.should == ["CC=CC[NH3+]", "O"]
-
-        mol = Rubabel["NC(O)CC"] 
-        fragments = mol.fragment( rules: [:sp3c_oxygen_double_bond_water_loss] )
-        fragments.flatten(1).map(&:csmiles).sort.should == []
-      end
-    end
-
-    describe 'sp3c_oxygen_double_bond_far_side_sp2' do
-
-      it 'does not cleave esters without sp3 carbons available for double bond' do
-        mol = Rubabel["NCCC(=O)OC"]
-        pieces = mol.fragment( rules: [:sp3c_oxygen_double_bond_far_side_sp2] )
-        pieces.should be_empty
+      describe 'peroxide carbonyl formation' do
       end
 
-      it 'cleaves esters on far side of singly bonded oxygen' do
-        mol = Rubabel["NCCC(=O)OCC"]
-        pieces = mol.fragment( rules: [:sp3c_oxygen_double_bond_far_side_sp2] )
-        pieces.size.should == 1  # one set
-        the_pair = pieces.first
-        csmiles = the_pair.map(&:csmiles)
-        csmiles.should include("OC(=O)CC[NH3+]")
-        csmiles.should include("C=C")
+      describe 'ether to alcohol' do
       end
 
-    end
+      describe 'ester to alcohol' do
+      end
 
-    describe ':alcohol_to_aldehyde' do
-      it 'cleaves beside alcohols to generate an aldehyde' do
-        mol = Rubabel["NCCC(O)CC"]
-        mol.correct_for_ph!
-        total_mass = mol.add_h!.mass
-
-        pieces = mol.fragment(rules: [:alcohol_to_aldehyde])
-        pieces.size.should == 2
-        pieces.map(&:size).should == [2,2]
-        pieces.flatten(1).map(&:csmiles).should == ["CC[NH3+]", "CCC=O", "C(C=O)C[NH3+]", "CC"]
-        pieces.each do |pair|
-          pair.map(&:mass).reduce(:+).should == total_mass
-        end
+      describe 'phosphodiester' do
       end
     end
 
   end
 end
+
+
+
+
+    #describe ':sp3c_nitrogen_double_bond' do
+
+      #it 'cleaves like an ether a secondary NH group if possible' do
+        #mol = Rubabel["CCNC"]
+        #frag_sets = mol.fragment(rules: [:sp3c_nitrogen_double_bond])
+        #frag_sets.size.should == 1
+        #csmiles = frag_sets.first.map(&:csmiles)
+        #csmiles.should include("C=C")
+        #csmiles.should include("C[NH3+]")
+      #end
+
+      #it 'will not cleave if not possible' do
+        #mol = Rubabel["CNC"]
+        #frag_sets = mol.fragment(rules: [:sp3c_nitrogen_double_bond])
+        #frag_sets.should be_empty
+      #end
+
+    #end
+
+    #describe ':co2_loss' do
+      #it 'loss of CO2 from carboxy group with charge transfer' do
+        #mol = Rubabel["NCC(=O)O"]
+        #frag_sets = mol.fragment( rules: [:co2_loss] )
+        #frag_sets.size.should == 1
+        #csmiles = frag_sets.first.map(&:csmiles)
+
+        #csmiles.should include("[CH2-][NH3+]")
+        #csmiles.should include("O=C=O")
+      #end
+
+      #it "doesn't remove CO2 if adjacent is not c3" do
+        #mol = Rubabel["C=CC(=O)O"]
+        #fragments = mol.fragment( rules: [:co2_loss] )
+        #fragments.should be_empty
+      #end
+
+    #end
+
+    #describe ':peroxy_to_carboxy' do
+      #it 'works' do
+        #mol = Rubabel["NCCC(OO)CC"]
+        #frag_sets = mol.fragment( rules: [:peroxy_to_carboxy] )
+        #frag_sets.size.should == 2 
+        #frag_sets.flatten(1).map(&:csmiles).sort.should == ["CC", "CCC(=O)O", "CC[NH3+]", "OC(=O)CC[NH3+]"]
+      #end
+    #end
+
+    #describe ':sp3c_oxygen_asymmetric_far_sp3', :pending do
+      #it 'splits like sp3c_oxygen_double_bond except oxygen takes the electrons' do
+        #$VERBOSE = 3
+        #mol = Rubabel["NCCCOCC"]
+        #frag_sets = mol.fragment( rules: [:sp3c_oxygen_asymmetric_far_sp3] )
+        #$VERBOSE = nil
+        #frag_sets.size.should == 2
+        ##mol = Rubabel["NCCOCC"]
+        ##p mol.fragment( rules: [:sp3c_oxygen_asymmetric_far_sp3] )
+        ##mol = Rubabel["NCOC"] 
+        ##p mol.fragment( rules: [:sp3c_oxygen_asymmetric_far_sp3] )
+      #end
+    #end
+
+    #describe ':sp3c_oxygen_double_bond_water_loss' do
+
+      #it 'does h2o loss of alcohol' do
+        #mol = Rubabel["NCCC(O)CC"]
+        #fragments = mol.fragment( rules: [:sp3c_oxygen_double_bond_water_loss] )
+        #fragments.flatten(1).map(&:csmiles).sort.should == ["CC=CCC[NH3+]", "CCC=CC[NH3+]", "O", "O"]
+      #end
+
+      #it 'h2o loss does not allow bad chemistry' do
+        ## lone pair and double bond resonance ?
+        #mol = Rubabel["NCC(O)CC"]
+        #fragments = mol.fragment( rules: [:sp3c_oxygen_double_bond_water_loss] )
+        #fragments.flatten(1).map(&:csmiles).sort.should == ["CC=CC[NH3+]", "O"]
+
+        #mol = Rubabel["NC(O)CC"] 
+        #fragments = mol.fragment( rules: [:sp3c_oxygen_double_bond_water_loss] )
+        #fragments.flatten(1).map(&:csmiles).sort.should == []
+      #end
+    #end
+
+    #describe 'sp3c_oxygen_double_bond_far_side_sp2' do
+
+      #it 'does not cleave esters without sp3 carbons available for double bond' do
+        #mol = Rubabel["NCCC(=O)OC"]
+        #pieces = mol.fragment( rules: [:sp3c_oxygen_double_bond_far_side_sp2] )
+        #pieces.should be_empty
+      #end
+
+      #it 'cleaves esters on far side of singly bonded oxygen' do
+        #mol = Rubabel["NCCC(=O)OCC"]
+        #pieces = mol.fragment( rules: [:sp3c_oxygen_double_bond_far_side_sp2] )
+        #pieces.size.should == 1  # one set
+        #the_pair = pieces.first
+        #csmiles = the_pair.map(&:csmiles)
+        #csmiles.should include("OC(=O)CC[NH3+]")
+        #csmiles.should include("C=C")
+      #end
+
+    #end
+
+    #describe ':alcohol_to_aldehyde' do
+      #it 'cleaves beside alcohols to generate an aldehyde' do
+        #mol = Rubabel["NCCC(O)CC"]
+        #mol.correct_for_ph!
+        #total_mass = mol.add_h!.mass
+
+        #pieces = mol.fragment(rules: [:alcohol_to_aldehyde])
+        #pieces.size.should == 2
+        #pieces.map(&:size).should == [2,2]
+        #pieces.flatten(1).map(&:csmiles).should == ["CC[NH3+]", "CCC=O", "C(C=O)C[NH3+]", "CC"]
+        #pieces.each do |pair|
+          #pair.map(&:mass).reduce(:+).should == total_mass
+        #end
+      #end
+    #end
